f853958a2b21dab258bb679e83862b9122349ca3
Elina Lulle <elulle@outlook.com>
2017-11-30 14:20:04 +0000

    APG13-addedsearch to news source (#4)

--- scripts/controllers/news-feed-controller.js
+++ scripts/controllers/news-feed-controller.js
@@ -2,11 +2,7 @@
 angular.module('newsApp')
 .controller('sourceCtrl', function($scope, newsDataService) {
     newsDataService.getSource(function(response){
-        var allSources = response.data.sources,
-            size = 3;
-        $scope.newsSourceList = [];
-        while (allSources.length > 0)
-            $scope.newsSourceList.push(allSources.splice(0, size));
+        $scope.newsSource = response.data.sources;
     });
 })
 .controller('articleCtrl', function($scope, articleService){
